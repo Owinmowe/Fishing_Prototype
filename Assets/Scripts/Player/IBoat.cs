a78303ce29eb2ee21ac338151271b63d14adcfba
Adrian Sgro <Owinmowe@gmail.com>
2023-04-06 11:48:07 -0300

    Add boat logic for fishing interaction

--- Assets/Scripts/Player/IBoat.cs
+++ Assets/Scripts/Player/IBoat.cs
@@ -1,8 +1,16 @@
+using FishingPrototype.Gameplay.FishingSpot;
+
 namespace FishingPrototype.Gameplay.Boat
 {
     public interface IBoat
     {
+        public static System.Action<IBoat> onLocalBoatSet; 
+        public event System.Action<IFishingSpot> OnFishingActionStarted;
+        public event System.Action OnFishingActionFailed;
+        public event System.Action OnFishingActionCanceled;
         void ReceiveAcceleration(float accelerationRate);
         void ReceiveRotation(float rotationRate);
+        void TryFishing();
+        void CancelFishing();
     }
 }
--- Assets/Scripts/Player/LocalBoat.cs
+++ Assets/Scripts/Player/LocalBoat.cs
@@ -1,60 +1,114 @@
+using System;
+using FishingPrototype.Gameplay.FishingSpot;
 using UnityEngine;
 
 namespace FishingPrototype.Gameplay.Boat
 {
     public class LocalBoat : MonoBehaviour, IBoat
     {
+        public event Action<IFishingSpot> OnFishingActionStarted;
+        public event Action OnFishingActionFailed;
+        public event Action OnFishingActionCanceled;
 
         [Header("Base Movement Configurations")] 
         [SerializeField] private float accelerationSpeed = 1f;
         [SerializeField] private float rotationSpeed = 1f;
+
+        [Header("Fishing Configurations")]
+        [SerializeField] private float fishingDistance = 5f;
+        [SerializeField] private LayerMask fishingLayerMask;
         
         private Rigidbody _rigidbody;
         
-        private bool accelInThisFixedDelta = false;
-        private float accelerationFloat = 0;
+        private bool _accelInThisFixedDelta = false;
+        private float _accelerationFloat = 0;
 
-        private bool rotateInThisFixedDelta = false;
-        private float rotationFloat = 0;
+        private bool _rotateInThisFixedDelta = false;
+        private float _rotationFloat = 0;
+
+        private const int MAX_FISHING_COLLIDERS_SIZE = 20;
+        private IFishingSpot _currentFishingSpot;
+        private readonly Collider[] _fishingColliders = new Collider[MAX_FISHING_COLLIDERS_SIZE];
         
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody>();
         }
 
+        private void Start()
+        {
+            IBoat.onLocalBoatSet?.Invoke(this);
+        }
+
+        private void FixedUpdate()
+        {
+            if (_accelInThisFixedDelta)
+                Accelerate(transform.right * _accelerationFloat);
+            
+            if(_rotateInThisFixedDelta)
+                Rotate(transform.up * _rotationFloat);
+        }
+
         public void ReceiveAcceleration(float accelerationRate)
         {
-            accelerationFloat = Mathf.Clamp(accelerationRate, -1, 1) * accelerationSpeed;
-            accelInThisFixedDelta = true;
+            if (_currentFishingSpot != null) return;
+            
+            _accelerationFloat = Mathf.Clamp(accelerationRate, -1, 1) * accelerationSpeed;
+            _accelInThisFixedDelta = true;
             
         }
 
         public void ReceiveRotation(float rotationRate)
         {
-            rotationFloat = Mathf.Clamp(rotationRate, -1, 1) * rotationSpeed;
-            rotateInThisFixedDelta = true;
+            if (_currentFishingSpot != null) return;
             
-        }
-
-        private void FixedUpdate()
-        {
-            if (accelInThisFixedDelta)
-                Accelerate(transform.right * accelerationFloat);
-            
-            if(rotateInThisFixedDelta)
-                Rotate(transform.up * rotationFloat);
+            _rotationFloat = Mathf.Clamp(rotationRate, -1, 1) * rotationSpeed;
+            _rotateInThisFixedDelta = true;
         }
 
         private void Accelerate(Vector3 force)
         {
             _rigidbody.AddForce(force, ForceMode.Acceleration);
-            accelInThisFixedDelta = false;
+            _accelInThisFixedDelta = false;
         }
 
         private void Rotate(Vector3 torqueForce)
         {
             _rigidbody.AddTorque(torqueForce, ForceMode.Acceleration);
-            rotateInThisFixedDelta = false;
+            _rotateInThisFixedDelta = false;
+        }
+        
+        public void TryFishing()
+        {
+            if (_currentFishingSpot != null) return;
+            
+            int collidersSize = Physics.OverlapSphereNonAlloc(transform.position, fishingDistance, _fishingColliders, fishingLayerMask);
+            for (int i = 0; i < collidersSize; i++)
+            {
+                IFishingSpot fishingSpot = _fishingColliders[i].GetComponent<IFishingSpot>();
+                if (fishingSpot != null)
+                {
+                    _currentFishingSpot = fishingSpot;
+                    _currentFishingSpot.OnFishingRequestProcessed += OnFishingRequestProcessed;
+                    _currentFishingSpot.TryFishing();
+                }
+            }
+        }
+
+        private void OnFishingRequestProcessed(bool okToFish)
+        {
+            _currentFishingSpot.OnFishingRequestProcessed -= OnFishingRequestProcessed;
+            if(okToFish) OnFishingActionStarted?.Invoke(_currentFishingSpot);
+            else OnFishingActionFailed?.Invoke();
+        }
+
+        public void CancelFishing()
+        {
+            if (_currentFishingSpot == null) return;
+            
+            _currentFishingSpot.OnCanceledFishing();
+            _currentFishingSpot = null;
+            OnFishingActionCanceled?.Invoke();
         }
     }
 }
