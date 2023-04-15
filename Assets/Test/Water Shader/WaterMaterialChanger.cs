ba53bc69ca59c03c25d0ac45118259d1ad53fabd
Adrian Sgro <Owinmowe@gmail.com>
2023-04-15 12:45:30 -0300

    Improve water shader test with more data in the scene

--- Assets/Test/Water Shader/WaterMaterialChanger.cs
+++ Assets/Test/Water Shader/WaterMaterialChanger.cs
@@ -1,42 +1,54 @@
-using System;
 using System.Collections.Generic;
 using FishingPrototype.Gameplay.Input;
 using FishingPrototype.Waves;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 namespace FishingPrototype.Test
 {
     public class WaterMaterialChanger : MonoBehaviour
     {
         [SerializeField] private MeshRenderer meshRenderer;
-        [SerializeField] private List<Material> materials;
+        [SerializeField] private List<MaterialData> materialsData;
+        [SerializeField] private TextMeshProUGUI materialNameComponent;
+        [SerializeField] private TextMeshProUGUI materialDescriptionComponent;
         private int materialIndex = 0;
-
-        private void Awake()
-        {
-            meshRenderer.material = materials[materialIndex];
-        }
 
         private void Start()
         {
-            WaveManager.Get().UpdateMaterialReference();
-            CustomInput.Input.TestControl.TestButton1.performed += ChangeMaterial;
+            SetNewMaterial();
+            CustomInput.Input.TestControl.TestButton1.performed += ChangeMaterialIndex;
         }
 
         private void OnDestroy()
         {
-            CustomInput.Input.TestControl.TestButton1.performed -= ChangeMaterial;
+            CustomInput.Input.TestControl.TestButton1.performed -= ChangeMaterialIndex;
         }
 
-        private void ChangeMaterial(InputAction.CallbackContext callback)
+        private void ChangeMaterialIndex(InputAction.CallbackContext callback)
         {
             materialIndex++;
-            if (materialIndex == materials.Count)
+            if (materialIndex == materialsData.Count)
                 materialIndex = 0;
-            
-            meshRenderer.material = materials[materialIndex];
+
+            SetNewMaterial();
+        }
+
+        private void SetNewMaterial()
+        {
+            meshRenderer.material = materialsData[materialIndex].material;
+            materialNameComponent.text = materialsData[materialIndex].material.name;
+            materialDescriptionComponent.text = materialsData[materialIndex].materialDescription;
             WaveManager.Get().UpdateMaterialReference();
+        }
+        
+        [System.Serializable]
+        public struct MaterialData
+        {
+            public Material material;
+            [SerializeField] public string materialDescription;
         }
     }
 }
