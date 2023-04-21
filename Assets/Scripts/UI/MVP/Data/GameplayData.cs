3a264524296a46b7885628a5d956e32185a6e62b
Adrian Sgro <Owinmowe@gmail.com>
2023-04-21 20:47:09 -0300

    Add MVP data for all presenters

--- Assets/Scripts/UI/MVP/Data/GameplayData.cs
+++ Assets/Scripts/UI/MVP/Data/GameplayData.cs
@@ -1,7 +1,11 @@
+using FishingPrototype.Gameplay.Minigames.Data;
+using UnityEngine;
+
 namespace FishingPrototype.MVP.Data
 {
+    [CreateAssetMenu(fileName = "Gameplay Data", menuName = "MVP/Gameplay Data", order = 1)]
     public class GameplayData : ScriptableData
     {
-
+        public MiniGamesData miniGamesData;
     }
 }
--- Assets/Scripts/UI/MVP/Data/LobbyData.cs
+++ Assets/Scripts/UI/MVP/Data/LobbyData.cs
@@ -1,5 +1,8 @@
+using UnityEngine;
+
 namespace FishingPrototype.MVP.Data
 {
+    [CreateAssetMenu(fileName = "Lobby Data", menuName = "MVP/Lobby Data", order = 1)]
     public class LobbyData : ScriptableData
     {
 
