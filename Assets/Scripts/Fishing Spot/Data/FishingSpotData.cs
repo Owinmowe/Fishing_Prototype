de1395cdb7e554cd536ea3a65e7f88024dba1968
Adrian Sgro <Owinmowe@gmail.com>
2023-05-06 23:27:33 -0300

    Reorganize namespaces

---
+++ Assets/Scripts/Fishing Spot/Data/FishingSpotData.cs
@@ -0,0 +1,9 @@
+namespace FishingPrototype.Gameplay.FishingSpot.Data
+{
+    public struct FishingSpotData
+    {
+        public int amount;
+        public int spawnIndex;
+        public FishingSpotType type;
+    }
+}
